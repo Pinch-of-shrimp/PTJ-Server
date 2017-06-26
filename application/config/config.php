773a7333af5289f376879580ca5bc207237cded6
kelele67 <kingand67@outlook.com>
2017-06-26 14:14:32 +0800

    abort PTJ-AliYun branch

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PTJ-Server/'; 
+$config['base_url'] = '120.24.72.112/PTJ-Server/'; 
 
 /*
 |--------------------------------------------------------------------------
