43d5857d73f7f8e0c177350c4f8c2312b69330ee
kelele67 <Kingand67@icloud.com>
2017-06-26 11:18:15 +0800

    connected aliyun

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PTJ-Server/'; 
+
+$config['base_url'] = '120.24.72.112/PTJ-Server/'; 
+
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +37,7 @@ $config['base_url'] = 'http://localhost/PTJ-Server/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
